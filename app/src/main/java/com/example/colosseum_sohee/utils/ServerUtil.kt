44ddc565661d84353344919b9748a5906302a6ba
Sohee <sohee3480@gmail.com>
2021-06-18 15:31:13 +0900

    서버의 응답을 처리하는 코드를 담는 변수, handler(인터페이스) 생성

--- app/src/main/java/com/example/colosseum_sohee/utils/ServerUtil.kt
+++ app/src/main/java/com/example/colosseum_sohee/utils/ServerUtil.kt
@@ -10,12 +10,16 @@ class ServerUtil {
     //    어떤 내용 => 서버 연결 전담
     companion object {
 
+        interface JsonResponseHandler {
+            fun onResponse(jsonObj : JSONObject)
+        }
+
         //    모든 기능의 기본이 되는 주소
         val BASE_URL = "http://54.180.52.26"
 
 //        로그인 하는 기능
 
-        fun postRequestLogin(email: String, pw: String) {
+        fun postRequestLogin(email: String, pw: String, handler : JsonResponseHandler?) {
 
 //            서버에 입력받은 email, pw 전달 => 로그인 기능 POST /user 로 전달 => 요청(Request) 실행
 //            라이브러리 (OkHttp) 활용해서 짜보기
@@ -57,8 +61,10 @@ class ServerUtil {
                     val jsonObj = JSONObject(bodyString)
 
                 Log.d("응답 본문", bodyString)
-                }
 
+//                    handler 변수가 null이 아니라면 (실체가 있다면) - 그 내부에 적힌 내용 실행
+                    handler?.onResponse(jsonObj)
+                }
             }
             )
 
