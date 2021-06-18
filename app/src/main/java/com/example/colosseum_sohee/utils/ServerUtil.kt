18f397f3880bd58fb348c40bfc70705797e4327b
Sohee <sohee3480@gmail.com>
2021-06-18 15:15:02 +0900

    ServerUtil 클래스 기능 추가  - 로그인을 실제로 실행시켜주는 기능

--- app/src/main/java/com/example/colosseum_sohee/utils/ServerUtil.kt
+++ app/src/main/java/com/example/colosseum_sohee/utils/ServerUtil.kt
@@ -1,5 +1,9 @@
 package com.example.colosseum_sohee.utils
 
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
 class ServerUtil {
 
     //    어떤 내용 => 서버 연결 전담
@@ -10,9 +14,35 @@ class ServerUtil {
 
 //        로그인 하는 기능
 
-        fun postRequestLogin() {
-            
-        }
+        fun postRequestLogin(email: String, pw: String) {
 
+//            서버에 입력받은 email, pw 전달 => 로그인 기능 POST /user 로 전달 => 요청(Request) 실행
+//            라이브러리 (OkHttp) 활용해서 짜보기
+
+//            http://54.180.52.26/user + POST + 파라미터들 첨부
+
+//            호스트 부소 + 기능 주소 결합
+            val urlString = "${BASE_URL}/user"
+
+//            POST 방식 => 파라미터를 폼 데이터에 담아주자 (짐 싸주기)
+
+            val formData = FormBody.Builder()
+                .add("email", email)
+                .add("password", pw)
+                .build()
+
+//            어디로(주소) 어떤 방식으로 무엇을 들고 갈것인지 ahen whdgkqgoens request 변수 생성
+            val request = Request.Builder()
+                .url(urlString)
+                .post(formData)
+                .build()
+
+//            클라이언트로써의 동작 : requst 요청 실행 => OkHttp 라이브러리 지원
+            val client = OkHttpClient()
+
+//            실제로 서버에 요청 날리기
+            client.newCall(request)
+
+        }
     }
 }
