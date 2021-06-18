8ab16b1ecf2addb8a548bc060fbaae0454d3c803
Sohee <sohee3480@gmail.com>
2021-06-18 16:18:44 +0900

    회원가입 성공시 닉네임 추출해서 토스트로

--- app/src/main/java/com/example/colosseum_sohee/SignUpActivity.kt
+++ app/src/main/java/com/example/colosseum_sohee/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.example.colosseum_sohee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.colosseum_sohee.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import org.json.JSONObject
@@ -26,6 +27,32 @@ class SignUpActivity : BaseActivity() {
             ServerUtil.putRequestSignUp(inputEmail, inputPw, inputNickname, object : ServerUtil.Companion.JsonResponseHandler{
                 override fun onResponse(jsonObj: JSONObject) {
 
+                    val code = jsonObj.getInt("code")
+
+                    if (code == 200) {
+
+//                        가입한 사람의 닉네임 추출 => 토스트로 '~님 환영합니다!' + 회원가입 화면 종류 => 로그인 화면으로 복귀
+
+//                        jsonObj{} => data{} => user{} => 내부에서 nick_name String 추출
+
+                        val dataObj = jsonObj.getJSONObject("data")
+                        val userObj = dataObj.getJSONObject("user")
+                        val nickName = userObj.getString("nick_name")
+
+                        runOnUiThread {
+                            Toast.makeText(mContext, "${nickName}님 환영합니다!", Toast.LENGTH_SHORT).show()
+                            finish()
+                        }
+                    }
+                    else {
+//                        실패 사유를 서버가 주는 message에 담긴 문구로 출력
+
+                        val message = jsonObj.getString("message")
+
+                        runOnUiThread {
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                        }
+                   }
                 }
 
 
