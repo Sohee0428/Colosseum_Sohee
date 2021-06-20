3aca3d6daeb5cab9d7bdee766b4ad0ded93908c8
Sohee <sohee3480@gmail.com>
2021-06-20 22:14:57 +0900

    자동 저장에 필요한 변수 만들기

---
+++ app/src/main/java/com/example/colosseum_sohee/utils/ContextUtil.kt
@@ -0,0 +1,13 @@
+package com.example.colosseum_sohee.utils
+
+class ContextUtil {
+
+    companion object {
+
+        private val prefName = "ColosseumPref"
+
+        private val TOKEN = "TOKEN"
+
+
+    }
+}
