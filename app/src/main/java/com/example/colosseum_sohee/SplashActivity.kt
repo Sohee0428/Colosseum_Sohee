48b1d30c1f29256b14c04bfdab3c735e4c465d6b
Sohee <sohee3480@gmail.com>
2021-06-20 22:36:54 +0900

    간단한 자동로그인 마무리

--- app/src/main/java/com/example/colosseum_sohee/SplashActivity.kt
+++ app/src/main/java/com/example/colosseum_sohee/SplashActivity.kt
@@ -1,7 +1,11 @@
 package com.example.colosseum_sohee
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.colosseum_sohee.utils.ContextUtil
 
 class SplashActivity : BaseActivity() {
 
@@ -17,5 +21,27 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        3초 후 검사 -> 저장된 토큰이 있는지  아니면 ""인지
+
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed({
+
+            if (ContextUtil.getToken(mContext)  == "") {
+
+//                저장된 토큰이 없다 => 로그인을 해야한다
+                val myIntent = Intent(mContext, LoginActivity::class.java)
+                startActivity(myIntent)
+            }
+            else {
+//                저장된 토큰이 있다 => 로그인이 되어있다 => 메인화면으로 이동
+                val myIntent = Intent(mContext, MainActivity::class.java)
+                startActivity(myIntent)
+            }
+
+            finish()
+
+        }, 3000)
     }
 }
