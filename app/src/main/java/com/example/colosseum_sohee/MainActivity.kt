7440b278b0061b6569c6dfe221771b803aea00a7
Sohee <sohee3480@gmail.com>
2021-06-28 21:25:52 +0900

    JSONObject 재료로 Topic으로 변환해주는 함수 추가

--- app/src/main/java/com/example/colosseum_sohee/MainActivity.kt
+++ app/src/main/java/com/example/colosseum_sohee/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.colosseum_sohee
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.colosseum_sohee.datas.Topic
+import com.example.colosseum_sohee.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -24,6 +26,26 @@ class MainActivity : BaseActivity() {
 
     fun getTopicListFromServer(){
 //        서버에서 주제 목록을 받아오자 => /v2/main_info => ServerUtil에 기능 추가 필요
+
+        ServerUtil.getRequestMainInfo(mContext, object : ServerUtil.Companion.JsonResponseHandler{
+            override fun onResponse(jsonObj: JSONObject) {
+//                서버에서 주제 목록을 받아온 상황
+
+                val dataObj = jsonObj.getJSONObject("data")
+                val topicArr = dataObj.getJSONArray("topics")
+
+//                topicArr 안에 있는 여러 개의 주제들을 반복적으로 파싱 => fon문을 활용
+//                배열에 10개의 주제가 있은경우 index는 0~9까지
+
+                for ( index in 0 until topicArr.length())
+
+//                    index 위치에 맞는 주제들을 Topics 클래스 형태로 변환
+//                    변환된 주제응 mTopicList에 추가
+
+
+            }
+
+       })
     }
 
 }
