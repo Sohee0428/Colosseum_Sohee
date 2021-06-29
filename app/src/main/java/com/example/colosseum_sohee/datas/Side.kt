d502d19489aeb857271884eb9d2b55893d17b9e0
Sohee <sohee3480@gmail.com>
2021-06-29 16:21:36 +0900

    주제 파싱 시 -> 선택 진영 + 득표 현황 같이 파싱

---
+++ app/src/main/java/com/example/colosseum_sohee/datas/Side.kt
@@ -0,0 +1,25 @@
+package com.example.colosseum_sohee.datas
+
+import org.json.JSONObject
+
+class Side {
+    var id = 0
+    var topicId = 0
+    var title = ""
+    var voteCount = 0
+
+    companion object{
+
+        fun getSideFromJson(jsonObject: JSONObject) : Side {
+
+            var resultSide = Side()
+
+            resultSide.id = jsonObject.getInt("id")
+            resultSide.topicId = jsonObject.getInt("topic_id")
+            resultSide.title = jsonObject.getString("title")
+            resultSide.voteCount = jsonObject.getInt("vote_count")
+
+            return resultSide
+        }
+    }
+}
