5f4de7dcddcfbc0beb6eb426b1085f5f77fc9969
Sohee <sohee3480@gmail.com>
2021-07-02 16:04:09 +0900

    어댑터 클래스 생성

---
+++ app/src/main/java/com/example/colosseum_sohee/adapters/ReplyAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.colosseum_sohee.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.example.colosseum_sohee.R
+import com.example.colosseum_sohee.datas.Reply
+import com.example.colosseum_sohee.datas.Topic
+
+class ReplyAdapter (val mContext: Context,
+                    resId: Int, // 다른 곳에서 사용하지 않을것이기 때문에 val 없이 작성
+                    val mList: List<Reply>) : ArrayAdapter<Reply>(mContext, resId, mList) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if(tempRow == null) {
+            tempRow = mInflater.inflate(R.layout.reply_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        val data = mList[position]
+
+
+
+        return row
+    }
+}
