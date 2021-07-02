db09ab514b01e035d3db8d71758e09999226a5c1
Sohee <sohee3480@gmail.com>
2021-07-02 15:56:44 +0900

    의견 보여주는 리스트뷰의 재료 생성

---
+++ app/src/main/java/com/example/colosseum_sohee/datas/Reply.kt
@@ -0,0 +1,21 @@
+package com.example.colosseum_sohee.datas
+
+import org.json.JSONObject
+
+class Reply {
+
+    var id = 0
+    var content = ""
+
+    companion object{
+
+        fun getReplyFromJson(jsonObj: JSONObject) : Reply{
+            val resultReply = Reply()
+
+            resultReply.id = jsonObj.getInt("id")
+            resultReply.content = jsonObj.getString("content")
+
+            return resultReply
+        }
+    }
+}
